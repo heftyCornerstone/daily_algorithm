97a6b883fb7e26727f327059b5f9984741ca6af3
sanha <heftycornerstone@gmail.com>
2025-04-02 15:31:07 +0900

    프로그래머스 Lv.2 n진수 게임

---
+++ solutions/programmers/level_2/nDigitNumbersGame.js
@@ -0,0 +1,22 @@
+function solution(n, t, m, p) {
+  let answer = "";
+  const endIndex = m * t - (m - p);
+  let nums = "";
+
+  for (let i = 0; i < m * t; i++) {
+    const convertedNumArr = i.toString(n).split("");
+
+    for (let k = 0; k < convertedNumArr.length; k++) {
+      if (nums.length === endIndex) break;
+
+      const curNum = convertedNumArr[k];
+      isNaN(Number(curNum)) ? (nums += curNum.toUpperCase()) : (nums += curNum);
+    }
+
+    if (nums.length === endIndex) break;
+  }
+
+  for (let i = p - 1; i < nums.length; i += m) answer += nums[i];
+
+  return answer;
+}
