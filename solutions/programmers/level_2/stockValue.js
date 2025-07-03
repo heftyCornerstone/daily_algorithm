1f4fae9037f95cb530df50d44b31b578a46cb763
sanha <heftycornerstone@gmail.com>
2025-07-03 18:48:54 +0900

    프로그래머스 Lv.2 주식가격

--- solutions/programmers/level_2/stockValue.js
+++ solutions/programmers/level_2/stockValue.js
@@ -5,6 +5,7 @@ function solution(prices) {
   for (let i = 0; i < prices.length; i++) {
     if (stack.length) {
       stack.forEach((c) => (answer[c] += 1));
+      // 현재값보다 비싼 가격을 스택에서 제거하기
       if (prices[stack[stack.length - 1]] > prices[i]) {
         stack = stack.filter((c) => prices[c] <= prices[i]);
       }
