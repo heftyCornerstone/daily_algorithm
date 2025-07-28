b26891e127d920401390ee60ffc6dd40f59febdb
sanha <heftycornerstone@gmail.com>
2025-07-28 15:59:49 +0900

    프로그래머스 Lv.2 2개 이하로 다른 비트

--- solutions/programmers/level_2/2orFewerDifferentBits.js
+++ solutions/programmers/level_2/2orFewerDifferentBits.js
@@ -1,27 +1,22 @@
 function solution(numbers) {
-  let answer = [];
+    let answer = [];
+    
+    numbers.forEach((c)=>{
+        const num = (c).toString(2);
+        const fullNum = '1'.padEnd(num.length, '1');
+        
+        let minimul = num;
+        if(num===fullNum) {
+            minimul = ('10').padEnd(num.length+1, '1');
+        } else {
+            const zeroIdx = num.lastIndexOf('0');
+            minimul = (num.substring(0, zeroIdx) + '1');
+            if(minimul.length<num.length) minimul += '0';
+            if(minimul.length<num.length) minimul += num.substring(zeroIdx+2);
+        }
 
-  numbers.forEach((c) => {
-    const num = c.toString(2);
-    const fullNum = '1'.padEnd(num.length, '1');
-
-    let minimul = num;
-    if (num === fullNum) {
-      minimul = '10'.padEnd(num.length + 1, '1');
-    } else {
-      for (let i = num.length - 1; i >= 0; i--) {
-        if (num[i] !== '0') continue;
-        let newMinimul = minimul.substring(0, i) + '1';
-        if (newMinimul.length < num.length) newMinimul += '0';
-        if (newMinimul.length < num.length) newMinimul += minimul.substring(i + 2);
-        minimul = newMinimul;
-
-        break;
-      }
-    }
-
-    answer.push(parseInt(minimul, 2));
-  });
-
-  return answer;
+        answer.push(parseInt(minimul, 2));
+    });
+    
+    return answer;
 }
